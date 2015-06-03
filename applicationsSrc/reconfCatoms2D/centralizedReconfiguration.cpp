9ba911d5eb2dc9a702693f02ecb15879aa8ca371
André Naz <andre.naz01@gmail.com>
2015-06-03 21:54:58 +0200

    start to implement a fourth strategy:  move first the node OUT_SHAPE on the perimeter that is closesr from the node WELL_PLACED that has the lowest neighbor cell IN_SHAPE to fill.

--- applicationsSrc/reconfCatoms2D/centralizedReconfiguration.cpp
+++ applicationsSrc/reconfCatoms2D/centralizedReconfiguration.cpp
@@ -17,9 +17,20 @@ using namespace Catoms2D;
 #define NB_MAX_CONSECUTIVE_NEIGHBORS_TO_MOVE 3
 //#define GRADIENT
 
-//#define STRATEGY_ONE
+/**
+ * Strategy:
+ * 1: let clockwise pivot moves first
+ * 2: let counter-clockwise pivot moves first
+ * 3: highest catom moves first
+ * 4: node OUT_SHAPE on the perimeter closer from the node IN_SHAPE that has
+ *    the lowest neighbor cell to fill.
+ **/
+
+
+//#Define STRATEGY_ONE
 #define STRATEGY_TWO
 //#define STRATEGY_THREE
+//#define STRATEGY_FOUR
 
 #define STRATEGY_TRHEE
 enum state_t {IN_SHAPE = 0, OUT_SHAPE = 1, WELL_PLACED = 2};
@@ -126,7 +137,7 @@ static P2PNetworkInterface *extremeNeighborInDirection(Catoms2DBlock *c, Catoms2
   
   p2 = p1;
   while (true) {
-      p2 = nextInterface(c, d, p2);
+    p2 = nextInterface(c, d, p2);
     if (!p2->connectedInterface) {
       return p1;
     }
@@ -134,15 +145,18 @@ static P2PNetworkInterface *extremeNeighborInDirection(Catoms2DBlock *c, Catoms2
   }
 }
 
-static P2PNetworkInterface *lastNeighborInDirection(Catoms2DBlock *c, Catoms2DMove::direction_t d) {
-  Catoms2DMove::direction_t d2 = Catoms2DMove::ROTATE_CW; // default
-  // the last one is the first one in the opposite direction
+static  Catoms2DMove::direction_t reverseDirection(Catoms2DMove::direction_t d) {
   if (d == Catoms2DMove::ROTATE_CCW) {
-    d2 = Catoms2DMove::ROTATE_CW;
+    return Catoms2DMove::ROTATE_CW;
   } else if (d == Catoms2DMove::ROTATE_CW) {
-    d2 = Catoms2DMove::ROTATE_CCW;
+    return Catoms2DMove::ROTATE_CCW;
   }
-  return extremeNeighborInDirection(c,d2);
+  return Catoms2DMove::ROTATE_CW; // default
+}
+
+static P2PNetworkInterface *lastNeighborInDirection(Catoms2DBlock *c, Catoms2DMove::direction_t d) {
+  // the last one is the first one in the opposite direction
+  return extremeNeighborInDirection(c,reverseDirection(d));
 }
 
 static P2PNetworkInterface *firstNeighborInDirection(Catoms2DBlock *c, Catoms2DMove::direction_t d) {
@@ -285,7 +299,7 @@ void centralized_reconfiguration() {
   Catoms2DWorld *world = Catoms2DWorld::getWorld();
   Catoms2DBlock *seed = NULL;
   int gradient[world->getSize()+1];
-  //state_t states[world->getSize()+1];
+  state_t states[world->getSize()+1];
   Tree *bfs = NULL;
 
   Coordinate mapBottomRight = getMapBottomRight();
@@ -296,10 +310,22 @@ void centralized_reconfiguration() {
   
   for (int i = 0; i < (world->getSize()+1); i++) {
     gradient[i] = UNDEFINED_GRADIENT;
-    //if (
-    //states[i] = 
+    states[i] = OUT_SHAPE;
+    //Catoms2DBlock *c = (Catoms2DBlock*) world->getBlockById(i);
   }
 
+  map<int, BuildingBlock*>::iterator it;
+  for (it=world->getMap().begin() ; it != world->getMap().end(); ++it) {
+    Catoms2DBlock *c = (Catoms2DBlock*) it->second;
+    Coordinate p(c->position[0],c->position[2]);
+    if (isInTarget(p)) {
+      states[c->blockId] = IN_SHAPE;
+     } else {
+      states[c->blockId] = OUT_SHAPE;
+    }
+  }
+  
+  
   bfs = Tree::bfs(seed->blockId,gradient,world->getSize());
 
   while (!isOver()) {
@@ -312,7 +338,7 @@ void centralized_reconfiguration() {
     // move CW around i connector: i+1 and i+2 should be free
     // move CCW around i connector: i-1 and i-2 should be free
     Catoms2DWorld *world = Catoms2DWorld::getWorld();
-    Catoms2DBlock  *c;
+    Catoms2DBlock *c;
 #if defined(STRATEGY_ONE) || defined (STRATEGY_TWO)
     map<int, BuildingBlock*>::iterator it;
     for (it=world->getMap().begin() ; it != world->getMap().end(); ++it) {
@@ -376,7 +402,7 @@ void centralized_reconfiguration() {
 	}
       }
     }
-    cout << "best: " << best << endl;
+    //cout << "best: " << best << endl;
     c = world->getGridPtr(best.x,0,best.y);
     Catoms2DMove *mv = nextMove(c);
     if (mv == NULL) { cout << "mv null" << endl;} 
@@ -391,6 +417,62 @@ void centralized_reconfiguration() {
     cout << " done"; //<< endl;
     getchar();
     delete mv;
+#elif defined(STRATEGY_FOUR)
+    // 4: node OUT_SHAPE on the perimeter closer from the node IN_SHAPE that has
+    // lowest neighbor cell to fill.
+    Coordinate lowest(INT_MIN,INT_MAX);
+    cout << "computing lowest" << endl;
+    map<int, BuildingBlock*>::iterator it;
+    for (it=world->getMap().begin() ; it != world->getMap().end(); ++it) {
+      
+      c = (Catoms2DBlock*) it->second;
+      Coordinate p1 = Coordinate(c->position[0],c->position[2]);
+      if (states[c->blockId] != IN_SHAPE) {
+	continue;
+      }
+
+      for (int i = 0; i < 6; i++) {
+	P2PNetworkInterface *p2p = c->getInterface((NeighborDirection::Direction)i);
+	if (!p2p->connectedInterface) {
+	  Coordinate p2 = Map::getPosition(c,p1,p2p);
+	  if (isInTarget(p2)) {
+	    if (p2.y < lowest.y) {
+	      lowest = p2;
+	    } else if (p2.y == lowest.y) {
+	      if (p2.x > lowest.x) {
+		lowest = p2;
+	      }
+	    }
+	  }
+	}
+      }
+    }
+    // we found the lowest cell to fill
+    // now, we follow the perimeter to found the closest OUT_SHAPE catoms
+    // (reverse order than the rotation direction)
+    cout << lowest << endl;
+    //Catoms2DMove::direction_t d = reverseDirection(ROTATION_DIRECTION);
+    c = world->getGridPtr(lowest.x,0,lowest.y);
+    P2PNetworkInterface *p2p = firstNeighborInDirection(c,ROTATION_DIRECTION);
+    Catoms2DBlock *cc = (Catoms2DBlock*) p2p->connectedInterface->hostBlock;
+#ifdef COLOR_DEBUG
+    c->setColor(RED);
+    cc->setColor(GREEN);
+#endif
+    /*bool willFillTheCell = false;
+    while (!willFillTheCell) {
+      if (canMove(cc,gradient)) {
+	Coordinate p1(cc->position[0], cc->position[2]);	  	
+	Catoms2DMove *mv = nextMove(c);
+	if (mv != NULL) {
+	  Coordinate p2 = getPosition(c,*mv);
+	  if (!isInTarget(p1) || (isInTarget(p1) && isInTarget(p2))) {
+	    
+	  }
+	  delete mv;
+      }
+      }*/
+    getchar();
 #endif
   }
   cout << "reconfiguration over in " << moves << " moves." << endl;
