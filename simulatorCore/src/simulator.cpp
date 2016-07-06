ProgrammableMatterProject/VisibleSim

commit 0a7e07e7083e42084f0cd78d7079671b35988a31

Enable MeldInterpret programs for all blocks

simulatorCore/src/simulator.cpp
@@ -5,6 +5,8 @@
  *      Author: dom
  */
 
+#include "simulator.h"
+
 #include "trace.h"
 #include "meldProcessVM.h"
 #include "meldProcessDebugger.h"
@@ -12,7 +14,7 @@
 #include "meldInterpretScheduler.h"
 #include "meldProcessScheduler.h"
 #include "cppScheduler.h"
-#include "simulator.h"
+#include "openGlViewer.h"
 
 using namespace std;
 

applicationsSrc/Makefile
@@ -1,5 +1,5 @@
 #SUBDIRS = noneCatoms2D bbCycle robot01 sphereCatoms3D sbReconf smartBlocks1 simpleCatoms3D simpleColorC2D simpleColorBB
-SUBDIRS = simpleCatoms3D simpleColorC2D simpleColorBB simpleColorSB distanceRB
+SUBDIRS = RBMeld C2DMeld C3DMeld blinkyMeld SBMeld
 # SUBDIRS =  blinkyMeld blinky01 bbCycle sbReconf coloration coloration_v2 leader_election smartBlocks1 detection map02 map03 map04 mElection01 reconfCatoms2D ABCcenterC2D csgCatoms3D sphereCatoms3D msrSync musicPlayer noneCatoms2D robot01 robot02 simpleCatoms3D simpleColorC2D simpleColorRB
 
 .PHONY: subdirs $(SUBDIRS) test

applicationsSrc/blinkyMeld/blinkyMeld.cpp
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
 	getScheduler()->printInfo();
 	BaseSimulator::getWorld()->printInfo();
 
-	getScheduler()->waitForSchedulerEnd();
+	// getScheduler()->waitForSchedulerEnd();
 
 	deleteSimulator();
 

applicationsSrc/blinkyMeld/blinkyMeldBlockCode.cpp
@@ -17,26 +17,15 @@ using namespace MeldInterpret;
 
 BlinkyMeldBlockCode::BlinkyMeldBlockCode(BlinkyBlocksBlock *host): BlinkyBlocksBlockCode(host) {
 	OUTPUT << "BlinkyMeldBlockCode constructor" << endl;
-	vm = new MeldInterpretVM((BlinkyBlocksBlock*)hostBlock);
+	vm = new MeldInterpretVM(hostBlock);
 	hasWork = true; // mode fastest
 	polling = false; // mode fastest
 	currentLocalDate = 0; // mode fastest
-	bb = (BlinkyBlocksBlock*) hostBlock;
-
-      if((vm != NULL)) {
-            for (int i = 0; i < NUM_PORTS; i++) {
-			vm->neighbors[i] = vm->get_neighbor_ID(i);
-			//OUTPUT << "Adding neighbor " << vm->neighbors[i] << " on face " << i << endl;
-
-			vm->enqueue_face(vm->neighbors[i], i, 1);
-		}
-            BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
-
-      }
+	bb = (BlinkyBlocksBlock*)hostBlock;
 }
 
 BlinkyMeldBlockCode::~BlinkyMeldBlockCode() {
-      delete vm;
+	delete vm;
 	OUTPUT << "BlinkyMeldBlockCode destructor" << endl;
 }
 
@@ -44,26 +33,25 @@ void BlinkyMeldBlockCode::init() {
 	stringstream info;
 
 	if((vm != NULL)) {
-            /*bb = (BlinkyBlocksBlock*) hostBlock;
-            for (int i = 0; i < NUM_PORTS; i++) {
+		bb = (BlinkyBlocksBlock*) hostBlock;
+		for (int i = 0; i < bb->getNbInterfaces(); i++) {
 			vm->neighbors[i] = vm->get_neighbor_ID(i);
 			OUTPUT << "Adding neighbor " << vm->neighbors[i] << " on face " << i << endl;
-
 			vm->enqueue_face(vm->neighbors[i], i, 1);
-		}*/
-            //block initialization
-		//setColor(0);
-		//vm->setLED(128,0,128,32);
-            //BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
+		}
+		
+		BaseSimulator::getScheduler()->schedule(
+			new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
 
 		if((getScheduler()->getMode() == SCHEDULER_MODE_FASTEST) && !vm->deterministicSet) {
 			vm->deterministicSet = true;
 			/*SetDeterministicModeVMCommand determinismCommand(c, bb->blockId);
-			vm->sendCommand(determinismCommand);*/
+			  vm->sendCommand(determinismCommand);*/
 			info << "deterministic mode set";
 			getScheduler()->trace(info.str(),hostBlock->blockId);
 			OUTPUT << "deterministic mode enable on the VM " << hostBlock->blockId << endl;
 		}
+
 	}
 }
 
@@ -79,11 +67,11 @@ void BlinkyMeldBlockCode::startup() {
 void BlinkyMeldBlockCode::handleDeterministicMode(/*VMCommand &command*/){
 	currentLocalDate = max(BaseSimulator::getScheduler()->now(), currentLocalDate);
 	/*if(!hasWork && (command.getType() != VM_COMMAND_STOP)) {
-		hasWork = true;
-#ifdef TEST_DETER
-		//cout << hostBlock->blockId << " has work again at " << BaseSimulator::getScheduler()->now() << endl;
-#endif
-	}*/
+	  hasWork = true;
+	  #ifdef TEST_DETER
+	  //cout << hostBlock->blockId << " has work again at " << BaseSimulator::getScheduler()->now() << endl;
+	  #endif
+	  }*/
 }
 
 void BlinkyMeldBlockCode::processLocalEvent(EventPtr pev) {
@@ -96,139 +84,140 @@ void BlinkyMeldBlockCode::processLocalEvent(EventPtr pev) {
 	cout << bb->blockId << " processLocalEvent: date: "<< BaseSimulator::getScheduler()->now() << " process event " << pev->getEventName() << "(" << pev->eventType << ")" << ", random number : " << pev->randomNumber << endl;
 #endif
 	switch (pev->eventType) {
-		case EVENT_COMPUTE_PREDICATE:
-		      {
-                        //Call the VM function to process one rule
-                        vm->processOneRule();
-                        //Add another compute event on condition
-                        //if...
-                        if(vm->isWaiting()){
-			  // random delay before recomputing a predicate
-			  // between 0.1 ms and 1ms
-			  int delay = (bb->getNextRandomNumber() % (1000 - 100 +1 )) + 100;
-			  BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now()+delay, bb));
-                        }
-                        //else info << " Block is no longer waiting";
-                        //info << "Compute predicate event";
-		      }
-			break;
-		case EVENT_STOP:
-			{
-				//getDebugger()->sendTerminateMsg(bb->blockId);
-				delete vm;
-                        info << "VM stopped";
-			}
-			break;
-		case EVENT_ADD_NEIGHBOR:
-			{
-			      //Should not be used by itself, presence of another block is tested by P2PNetworkInterface
-			      unsigned int face = (std::static_pointer_cast<AddNeighborEvent>(pev))->face;
-                        vm->neighbors[face] = (std::static_pointer_cast<AddNeighborEvent>(pev))->target;
-                        vm->enqueue_face(vm->neighbors[face], face, 1);
-                        BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
-                        info << "Add neighbor "<< (std::static_pointer_cast<AddNeighborEvent>(pev))->target << " at face " << BlinkyBlocks::NeighborDirection::getString(BlinkyBlocks::NeighborDirection::getOpposite((std::static_pointer_cast<AddNeighborEvent>(pev))->face));
-			}
-			break;
-		case EVENT_REMOVE_NEIGHBOR:
-			{
-			      unsigned int face = (std::static_pointer_cast<AddNeighborEvent>(pev))->face;
-                        vm->neighbors[face] = 0;
-                        vm->enqueue_face(vm->neighbors[face], face, -1);
-                        BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
-                        info << "Remove neighbor at face " << BlinkyBlocks::NeighborDirection::getString(BlinkyBlocks::NeighborDirection::getOpposite(face));
-			}
-			break;
-		case EVENT_TAP:
-			{
-			      vm->enqueue_tap();
-                        BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
-                        info << "tapped";
-			}
-			break;
-		case EVENT_SET_COLOR:
-			{
-                        bb->getTime();
-			      //Called by the VM, no need to enqueue things
-                        Color color = (std::static_pointer_cast<SetColorEvent>(pev))->color;
-                        bb->setColor(color);
+	case EVENT_COMPUTE_PREDICATE:
+	{
+		//Call the VM function to process one rule
+		vm->processOneRule();
+		//Add another compute event on condition
+		//if...
+		if(vm->isWaiting()){
+			// random delay before recomputing a predicate
+			// between 0.1 ms and 1ms
+			int delay = (bb->getNextRandomNumber() % (1000 - 100 +1 )) + 100;
+			BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now()+delay, bb));
+		}
+		//else info << " Block is no longer waiting";
+		//info << "Compute predicate event";
+	}
+	break;
+	case EVENT_STOP:
+	{
+		//getDebugger()->sendTerminateMsg(bb->blockId);
+		delete vm;
+		info << "VM stopped";
+	}
+	break;
+	case EVENT_ADD_NEIGHBOR:
+	{
+		//Should not be used by itself, presence of another block is tested by P2PNetworkInterface
+		unsigned int face = (std::static_pointer_cast<AddNeighborEvent>(pev))->face;
+		vm->neighbors[face] = (std::static_pointer_cast<AddNeighborEvent>(pev))->target;
+		vm->enqueue_face(vm->neighbors[face], face, 1);
+		BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
+		info << "Add neighbor "<< (std::static_pointer_cast<AddNeighborEvent>(pev))->target << " at face " << SCLattice::getString(SCLattice::getOpposite((std::static_pointer_cast<AddNeighborEvent>(pev))->face));
+	}
+	break;
+	case EVENT_REMOVE_NEIGHBOR:
+	{
+		unsigned int face = (std::static_pointer_cast<AddNeighborEvent>(pev))->face;
+		vm->neighbors[face] = VACANT;
+		vm->enqueue_face(vm->neighbors[face], face, -1);
+		BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
+		info << "Remove neighbor at face " << SCLattice::getString(SCLattice::getOpposite(face));
+	}
+	break;
+	case EVENT_TAP:
+	{
+		vm->enqueue_tap();
+		BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
+		info << "tapped, database dump printed to stderr...";
+		vm->facts_dump();
+	}
+	break;
+	case EVENT_SET_COLOR:
+	{
+		bb->getTime();
+		//Called by the VM, no need to enqueue things
+		Color color = (std::static_pointer_cast<SetColorEvent>(pev))->color;
+		bb->setColor(color);
 #ifdef TEST_DETER
-                        cout << bb->blockId << " SET_COLOR_EVENT" << endl;
+		cout << bb->blockId << " SET_COLOR_EVENT" << endl;
 #endif
-                        info << "set color "<< color;
-			}
-			break;
+		info << "set color "<< color;
+	}
+	break;
 
-            /*The interface being connected is tested in function tuple_send of the MeldInterpVM*/
-		case EVENT_SEND_MESSAGE:
-			{
-                        MessagePtr message = (std::static_pointer_cast<VMSendMessageEvent>(pev))->message;
-                        P2PNetworkInterface *interface = (std::static_pointer_cast<VMSendMessageEvent>(pev))->sourceInterface;
-                        getScheduler()->schedule(new NetworkInterfaceEnqueueOutgoingEvent(BaseSimulator::getScheduler()->now(), message, interface));
-                        //info << "sends a message at face " << NeighborDirection::getString(bb->getDirection(interface))  << " to " << interface->connectedInterface->hostBlock->blockId;
-			}
+	/*The interface being connected is tested in function tuple_send of the MeldInterpVM*/
+	case EVENT_SEND_MESSAGE:
+	{
+		MessagePtr message = (std::static_pointer_cast<VMSendMessageEvent>(pev))->message;
+		P2PNetworkInterface *interface = (std::static_pointer_cast<VMSendMessageEvent>(pev))->sourceInterface;
+		getScheduler()->schedule(new NetworkInterfaceEnqueueOutgoingEvent(BaseSimulator::getScheduler()->now(), message, interface));
+		//info << "sends a message at face " << NeighborDirection::getString(bb->getDirection(interface))  << " to " << interface->connectedInterface->hostBlock->blockId;
+	}
+	break;
+	case EVENT_RECEIVE_MESSAGE: /*EVENT_NI_RECEIVE: */
+	{
+		MessagePtr mes = (std::static_pointer_cast<NetworkInterfaceReceiveEvent>(pev))->message;
+		switch(mes->type){
+		case ADD_TUPLE_MSG_ID:
+			getScheduler()->schedule(new AddTupleEvent(BaseSimulator::getScheduler()->now(), hostBlock, std::static_pointer_cast<AddTupleMessage>(mes)->tuple, bb->getDirection(mes->sourceInterface->connectedInterface)));
 			break;
-		case EVENT_RECEIVE_MESSAGE: /*EVENT_NI_RECEIVE: */
-			{
-			      MessagePtr mes = (std::static_pointer_cast<NetworkInterfaceReceiveEvent>(pev))->message;
-                        switch(mes->type){
-                        case ADD_TUPLE_MSG_ID:
-                              getScheduler()->schedule(new AddTupleEvent(BaseSimulator::getScheduler()->now(), hostBlock, std::static_pointer_cast<AddTupleMessage>(mes)->tuple, bb->getDirection(mes->sourceInterface->connectedInterface)));
-                              break;
-                        case REMOVE_TUPLE_MSG_ID:
-                              getScheduler()->schedule(new RemoveTupleEvent(BaseSimulator::getScheduler()->now(), hostBlock, std::static_pointer_cast<RemoveTupleMessage>(mes)->tuple, bb->getDirection(mes->sourceInterface->connectedInterface)));
-                              break;
-                        }
-#ifdef TEST_DETER
-                        cout << "message received from " << command->sourceInterface->hostBlock->blockId << endl;
-#endif
-                        //info << "message received at face " << NeighborDirection::getString(bb->getDirection(mes->sourceInterface->connectedInterface)) << " from " << mes->sourceInterface->hostBlock->blockId;
-			}
-			break;
-		case EVENT_ACCEL:
-			{
-                        /*Not written yet, have to check how the vm handle accel (tuple, etc)*/
-                        info << "accel";
-			}
-			break;
-		case EVENT_SHAKE:
-			{
-			      /*Not written yet, same as accel*/
-
-                        info << "shake";
-			}
-			break;
-		case EVENT_SET_DETERMINISTIC:
-			{
-                        /*Not sure how to handle that with MeldInterp*/
-                        OUTPUT << "VM set in deterministic mode " << hostBlock->blockId << endl;
-                        info << "VM set in deterministic mode";
-			}
-			break;
-		case EVENT_END_POLL:
-			{
-                        polling = false;
-                        /*Not written yet
-                          Need to check what this is for*/
-                        info << "Polling time period ended";
-			}
-			break;
-            case EVENT_ADD_TUPLE:
-                        this->vm->receive_tuple(1, std::static_pointer_cast<AddTupleEvent>(pev)->tuple, std::static_pointer_cast<AddTupleEvent>(pev)->face);
-                        BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
-                        //info << "Adding tuple";
-                  break;
-            case EVENT_REMOVE_TUPLE:
-                        this->vm->receive_tuple(-1, std::static_pointer_cast<RemoveTupleEvent>(pev)->tuple, std::static_pointer_cast<RemoveTupleEvent>(pev)->face);
-                        BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
-                        //info << "Removing tuple";
-                  break;
-		default:
-			ERRPUT << "*** ERROR *** : unknown local event";
+		case REMOVE_TUPLE_MSG_ID:
+			getScheduler()->schedule(new RemoveTupleEvent(BaseSimulator::getScheduler()->now(), hostBlock, std::static_pointer_cast<RemoveTupleMessage>(mes)->tuple, bb->getDirection(mes->sourceInterface->connectedInterface)));
 			break;
 		}
-		if(info.str() != "") {
-                        getScheduler()->trace(info.str(),hostBlock->blockId);
-		}
+#ifdef TEST_DETER
+		cout << "message received from " << command->sourceInterface->hostBlock->blockId << endl;
+#endif
+		//info << "message received at face " << NeighborDirection::getString(bb->getDirection(mes->sourceInterface->connectedInterface)) << " from " << mes->sourceInterface->hostBlock->blockId;
+	}
+	break;
+	case EVENT_ACCEL:
+	{
+		/*Not written yet, have to check how the vm handle accel (tuple, etc)*/
+		info << "accel";
+	}
+	break;
+	case EVENT_SHAKE:
+	{
+		/*Not written yet, same as accel*/
+
+		info << "shake";
+	}
+	break;
+	case EVENT_SET_DETERMINISTIC:
+	{
+		/*Not sure how to handle that with MeldInterp*/
+		OUTPUT << "VM set in deterministic mode " << hostBlock->blockId << endl;
+		info << "VM set in deterministic mode";
+	}
+	break;
+	case EVENT_END_POLL:
+	{
+		polling = false;
+		/*Not written yet
+		  Need to check what this is for*/
+		info << "Polling time period ended";
+	}
+	break;
+	case EVENT_ADD_TUPLE:
+		this->vm->receive_tuple(1, std::static_pointer_cast<AddTupleEvent>(pev)->tuple, std::static_pointer_cast<AddTupleEvent>(pev)->face);
+		BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
+		//info << "Adding tuple";
+		break;
+	case EVENT_REMOVE_TUPLE:
+		this->vm->receive_tuple(-1, std::static_pointer_cast<RemoveTupleEvent>(pev)->tuple, std::static_pointer_cast<RemoveTupleEvent>(pev)->face);
+		BaseSimulator::getScheduler()->schedule(new ComputePredicateEvent(BaseSimulator::getScheduler()->now(), bb));
+		//info << "Removing tuple";
+		break;
+	default:
+		ERRPUT << "*** ERROR *** : unknown local event";
+		break;
+	}
+	if(info.str() != "") {
+		getScheduler()->trace(info.str(),hostBlock->blockId);
+	}
 }
 
 BlinkyBlocks::BlinkyBlocksBlockCode* BlinkyMeldBlockCode::buildNewBlockCode(BlinkyBlocksBlock *host) {

simulatorCore/src/blinkyBlocksBlock.cpp
@@ -16,6 +16,7 @@
 #include "clock.h"
 #include "meldProcessEvents.h"
 #include "meldInterpretEvents.h"
+#include "lattice.h"
 
 using namespace std;
 
@@ -25,16 +26,16 @@ using namespace std;
 
 namespace BlinkyBlocks {
 
-BlinkyBlocksBlock::BlinkyBlocksBlock(int bId, BlockCodeBuilder bcb) : BaseSimulator::BuildingBlock(bId, bcb) {
+BlinkyBlocksBlock::BlinkyBlocksBlock(int bId, BlockCodeBuilder bcb)
+	: BaseSimulator::BuildingBlock(bId, bcb, SCLattice::MAX_NB_NEIGHBORS) {
     OUTPUT << "BlinkyBlocksBlock constructor" << endl;
     double dataRateMin = ((BLINKYBLOCKS_PACKET_DATASIZE*pow(10,6)*8)
 						  / (BLINKYBLOCKS_TRANSMISSION_MAX_TIME*1000));
     double dataRateMax = ((BLINKYBLOCKS_PACKET_DATASIZE*pow(10,6)*8)
 						  / (BLINKYBLOCKS_TRANSMISSION_MIN_TIME*1000));
-
-    for (int i=0; i<6; i++) {
-		P2PNetworkInterface *p2p = new P2PNetworkInterface(this);
-		getP2PNetworkInterfaces().push_back(p2p);
+	
+    for (int i = 0; i < SCLattice::MAX_NB_NEIGHBORS; i++) {
+		P2PNetworkInterface *p2p = P2PNetworkInterfaces[i];
 		p2p->setDataRate((dataRateMax+dataRateMin)/2);
 		p2p->setDataRateVariability((dataRateMax-dataRateMin)/2);
     }

simulatorCore/src/buildingBlock.cpp
@@ -23,14 +23,16 @@ int BuildingBlock::nextId = 0;
 //
 //===========================================================================================================
 
-BuildingBlock::BuildingBlock(int bId, BlockCodeBuilder bcb) {
+BuildingBlock::BuildingBlock(int bId, BlockCodeBuilder bcb, int nbInterfaces) {
     OUTPUT << "BuildingBlock constructor (id:" << nextId << ")" << endl;
+	
     if (bId < 0) {
 		blockId = nextId;
 		nextId++;
     } else {
 		blockId = bId;
     }
+
     P2PNetworkInterfaceNextLocalId = 0;
     state.store(ALIVE);
     clock = NULL;
@@ -38,6 +40,11 @@ BuildingBlock::BuildingBlock(int bId, BlockCodeBuilder bcb) {
     generator = std::ranlux48(rd());
     dis = uniform_int_distribution<>(0, 50 * blockId);
     buildNewBlockCode = bcb;
+
+	for (int i = 0; i < nbInterfaces; i++) {
+        P2PNetworkInterfaces.push_back(new P2PNetworkInterface(this));
+    }
+	
 	blockCode = (BaseSimulator::BlockCode*)bcb(this);
 	isMaster = false;
 }
@@ -197,5 +204,25 @@ uint64_t BuildingBlock::getSchedulerTimeForLocalTime(uint64_t localTime) {
     }
     return clock->getSchedulerTimeForLocalTime(localTime);
 }
+
+/*************************************************
+ *            MeldInterpreter Functions  
+ *************************************************/
+
+unsigned short BuildingBlock::getNeighborIDForFace(int faceNum) {
+    short nodeID = P2PNetworkInterfaces[faceNum]->getConnectedBlockId();
+	
+	return nodeID > 0  ? (unsigned short)nodeID : 0;
+}
+
+int BuildingBlock::getFaceForNeighborID(int nId) {
+	for (int face = 0; face < P2PNetworkInterfaces.size(); face++) {
+		if (nId == getNeighborIDForFace(face))
+			return face;
+	}
+
+	return -1;
+}
+
 
 } // BaseSimulator namespace

simulatorCore/src/buildingBlock.h
@@ -79,8 +79,9 @@ public:
 	 * @brief BuildingBlock constructor
 	 * @param bId : the block id of the block to create
 	 * @param bcb : function pointer to the getter for the block's CodeBlock
+	 * @param nbInterfaces : number of initial interfaces of the block (Necessary for MeldInterpretVM init)
 	 */
-	BuildingBlock(int bId, BlockCodeBuilder bcb);
+	BuildingBlock(int bId, BlockCodeBuilder bcb, int nbInterfaces);
     /**
 	 * @brief BuildingBlock destructor
 	 */
@@ -159,16 +160,21 @@ public:
 	 */
 	inline Vector3D getPositionVector() { return Vector3D(position[0], position[1], position[2]);};
 	/**
-	 * @brief Adds block targer as neighbor for this block on interface ni
-	 * @param ni : pointer to the interface to connect
-	 * @param target : pointer to the BuildingBlock to connect to interface ni
+	 * @brief Schedules an AddNewNeighbor event
+	 * @param ni : pointer to the interface that was just connected
+	 * @param target : pointer to the BuildingBlock connected to interface ni
 	 */
-	virtual void addNeighbor(P2PNetworkInterface *ni, BuildingBlock* target) {};
+	virtual void addNeighbor(P2PNetworkInterface *ni, BuildingBlock* target) {};	
 	/**
-	 * @brief Disconnect interface ni
-	 * @param ni : pointer to the interface to disconnect
+	 * @brief Schedules a RemoveNeighborEvent
+	 * @param ni : pointer to the disconnected interface
 	 */
 	virtual void removeNeighbor(P2PNetworkInterface *ni) {};
+	/**
+	 * @brief Returns the number of interfaces for this block
+	 * @return number of interface for this block
+	 */
+	inline unsigned short getNbInterfaces() {	return P2PNetworkInterfaces.size(); };
 	/**
 	 * @brief Schedules a stop event for this block at a given date and update its state
 	 * @param date : date at which the stop event must be processed
@@ -208,6 +214,22 @@ public:
 	 * @return global time corresponding to the local time in parameter
 	 */   	
 	uint64_t getSchedulerTimeForLocalTime(uint64_t localTime);
+
+	/*************************************************
+	 *            MeldInterpreter Functions  
+	 *************************************************/
+	/**
+	 * @brief Returns the id of the block connected on interface #faceNum of this block
+	 * @param faceNum : id of the connected interface
+	 * @return id of the block connected to interface faceNum 
+	 */
+	unsigned short getNeighborIDForFace(int faceNum);
+	/**
+	 * @brief Returns the id of the face from this block connected to block of id nId
+	 * @param nId : id of the connected block 
+	 * @return the id of the face connected to block nId, or -1 if the two blocks are not neighbors
+	 */
+	int getFaceForNeighborID(int nId);
 };
 
 } // BaseSimulator namespace

simulatorCore/src/catoms2DBlock.cpp
@@ -18,12 +18,9 @@ using namespace std;
 
 namespace Catoms2D {
 
-Catoms2DBlock::Catoms2DBlock(int bId, BlockCodeBuilder bcb) : BaseSimulator::BuildingBlock(bId, bcb) {
+Catoms2DBlock::Catoms2DBlock(int bId, BlockCodeBuilder bcb)
+	: BaseSimulator::BuildingBlock(bId, bcb, SCLattice::MAX_NB_NEIGHBORS) {
     OUTPUT << "Catoms2DBlock constructor" << endl;
-
-    for (int i=0; i<HLattice::MAX_NB_NEIGHBORS; i++) {
-		getP2PNetworkInterfaces().push_back(new P2PNetworkInterface(this));
-    }
 
     angle = 0;
 }
@@ -310,6 +307,22 @@ void Catoms2DBlock::startMove(Catoms2DMove &m, uint64_t t) {
 
 void Catoms2DBlock::startMove(Catoms2DMove &m) {
     startMove(m,getScheduler()->now());
+}
+
+void Catoms2DBlock::addNeighbor(P2PNetworkInterface *ni, BuildingBlock* target) {
+    OUTPUT << "Simulator: "<< blockId << " add neighbor " << target->blockId << " on "
+		   << HLattice::getString(getDirection(ni)) << endl;
+    getScheduler()->scheduleLock(
+		new AddNeighborEvent(getScheduler()->now(), this,
+							 HLattice::getOpposite(getDirection(ni)), target->blockId));
+}
+
+void Catoms2DBlock::removeNeighbor(P2PNetworkInterface *ni) {
+    OUTPUT << "Simulator: "<< blockId << " remove neighbor on "
+		   << HLattice::getString(getDirection(ni)) << endl;
+    getScheduler()->scheduleLock(
+		new RemoveNeighborEvent(getScheduler()->now(), this,
+								HLattice::getOpposite(getDirection(ni))));
 }
 
 }

commit 85bef1e5bf03391549cd9bc20949e374f4e91700

Fix compilation issue on Linux introduced by 0a7e07e7083e42084f0cd78d7079671b35988a31

simulatorCore/src/simulator.cpp
@@ -14,7 +14,7 @@
 #include "meldInterpretScheduler.h"
 #include "meldProcessScheduler.h"
 #include "cppScheduler.h"
-#include "openGlViewer.h"
+#include "openglViewer.h"
 
 using namespace std;
 