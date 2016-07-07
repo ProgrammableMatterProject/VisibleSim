ProgrammableMatterProject/VisibleSim

commit f00e8c64aa4cffd24f0e5ba8124eb7bbf77643d3

Refactor SmartBlocks/RobotBlocks MotionEvents into more generic TranslationEvents

simulatorCore/src/translationEvent.h
@@ -1,17 +1,19 @@
-/*
- * smartBlocksEvents.h
+/**
+ * @file translationEvents.h
+ * @brief This file implements the realization of a translation motion events
+ *  (created from refactoring of smartBlocksEvents and robotBlocksEvents)
  *
- *  Created on: 2014 febrary 1st
- *      Author: Benoît
+ *  Created on: 07/07/16
+ *      Author: Pierre
  */
 
-#ifndef SMARTBLOCKSEVENTS_H_
-#define SMARTBLOCKSEVENTS_H_
+#ifndef TRANSLATIONEVENTS_H_
+#define TRANSLATIONEVENTS_H_
 
-#include "smartBlocksBlock.h"
+#include "BuildingBlock.h"
 #include "events.h"
 
-namespace SmartBlocks {
+namespace BaseSimulator {
 
 //===========================================================================================================
 //
@@ -22,7 +24,7 @@ namespace SmartBlocks {
 class MotionStartEvent : public BlockEvent {
     Vector3D finalPosition;
 public:
-    MotionStartEvent(uint64_t, SmartBlocksBlock *block,const Vector3D &fpos);
+    MotionStartEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
     MotionStartEvent(MotionStartEvent *ev);
     ~MotionStartEvent();
     void consumeBlockEvent() {};
@@ -39,11 +41,11 @@ public:
 class MotionStepEvent : public BlockEvent {
     Vector3D finalPosition;	//!< Target position of the block
     Vector3D motionStep;		//!< Motion of the block during this step
-    Vector3D motionPosition;		//!< Actual position on the grid of the block in motionPosition
+    Vector3D motionPosition;		//!< Actual position on the grid of the block in motion
 public:
-    MotionStepEvent(uint64_t, SmartBlocksBlock *block,const Vector3D &fpos);
-    MotionStepEvent(uint64_t, SmartBlocksBlock *block,const Vector3D &fpos,
-		    const Vector3D &step,const Vector3D &motionPosition);
+    MotionStepEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
+    MotionStepEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos,
+		    const Vector3D &step, const Vector3D &mpos);
     MotionStepEvent(MotionStepEvent *ev);
     ~MotionStepEvent();
     void consumeBlockEvent() {};
@@ -60,7 +62,7 @@ public:
 class MotionStopEvent : public BlockEvent {
     Vector3D finalPosition;
 public:
-    MotionStopEvent(uint64_t, SmartBlocksBlock *block,const Vector3D &fpos);
+    MotionStopEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
     MotionStopEvent(MotionStepEvent *ev);
     ~MotionStopEvent();
     void consumeBlockEvent() {};
@@ -76,7 +78,7 @@ public:
 
 class MotionEndEvent : public BlockEvent {
 public:
-    MotionEndEvent(uint64_t, SmartBlocksBlock *block);
+    MotionEndEvent(uint64_t, BuildingBlock *block);
     MotionEndEvent(MotionEndEvent *ev);
     ~MotionEndEvent();
     void consumeBlockEvent() {};
@@ -85,4 +87,4 @@ public:
 };
 
 }
-#endif /* ROBOTBLOCKSEVENTS_H_ */
+#endif /* TRANSLATIONEVENTS_H_ */

simulatorCore/src/translationEvents.h
@@ -1,30 +1,32 @@
-/*
- * robotBlocksEvents.h
+/**
+ * @file translationEvents.h
+ * @brief This file implements the realization of a translation motion events
+ *  (created from refactoring of smartBlocksEvents and robotBlocksEvents)
  *
- *  Created on: 2014 febrary 1st
- *      Author: Benoît
+ *  Created on: 07/07/16
+ *      Author: Pierre
  */
 
-#ifndef ROBOTBLOCKSEVENTS_H_
-#define ROBOTBLOCKSEVENTS_H_
+#ifndef TRANSLATIONEVENTS_H_
+#define TRANSLATIONEVENTS_H_
 
-#include "robotBlocksBlock.h"
+#include "BuildingBlock.h"
 #include "events.h"
 
-namespace RobotBlocks {
+namespace BaseSimulator {
 
 //===========================================================================================================
 //
-//          MotionStartEvent  (class)
+//          TranslationStartEvent  (class)
 //
 //===========================================================================================================
 
-class MotionStartEvent : public BlockEvent {
+class TranslationStartEvent : public BlockEvent {
     Vector3D finalPosition;
 public:
-    MotionStartEvent(uint64_t, RobotBlocksBlock *block,const Vector3D &fpos);
-    MotionStartEvent(MotionStartEvent *ev);
-    ~MotionStartEvent();
+    TranslationStartEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
+    TranslationStartEvent(TranslationStartEvent *ev);
+    ~TranslationStartEvent();
     void consumeBlockEvent() {};
     void consume();
     const virtual string getEventName();
@@ -32,20 +34,20 @@ public:
 
 //===========================================================================================================
 //
-//          MotionStepEvent  (class)
+//          TranslationStepEvent  (class)
 //
 //===========================================================================================================
 
-class MotionStepEvent : public BlockEvent {
+class TranslationStepEvent : public BlockEvent {
     Vector3D finalPosition;	//!< Target position of the block
-    Vector3D motionStep;		//!< Motion of the block during this step
+    Vector3D motionStep;		//!< Translation of the block during this step
     Vector3D motionPosition;		//!< Actual position on the grid of the block in motion
 public:
-    MotionStepEvent(uint64_t, RobotBlocksBlock *block,const Vector3D &fpos);
-    MotionStepEvent(uint64_t, RobotBlocksBlock *block,const Vector3D &fpos,
+    TranslationStepEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
+    TranslationStepEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos,
 		    const Vector3D &step, const Vector3D &mpos);
-    MotionStepEvent(MotionStepEvent *ev);
-    ~MotionStepEvent();
+    TranslationStepEvent(TranslationStepEvent *ev);
+    ~TranslationStepEvent();
     void consumeBlockEvent() {};
     void consume();
     const virtual string getEventName();
@@ -53,16 +55,16 @@ public:
 
 //===========================================================================================================
 //
-//          MotionStopEvent  (class)
+//          TranslationStopEvent  (class)
 //
 //===========================================================================================================
 
-class MotionStopEvent : public BlockEvent {
+class TranslationStopEvent : public BlockEvent {
     Vector3D finalPosition;
 public:
-    MotionStopEvent(uint64_t, RobotBlocksBlock *block,const Vector3D &fpos);
-    MotionStopEvent(MotionStepEvent *ev);
-    ~MotionStopEvent();
+    TranslationStopEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
+    TranslationStopEvent(TranslationStepEvent *ev);
+    ~TranslationStopEvent();
     void consumeBlockEvent() {};
     void consume();
     const virtual string getEventName();
@@ -70,19 +72,19 @@ public:
 
 //===========================================================================================================
 //
-//          MotionEndEvent  (class)
+//          TranslationEndEvent  (class)
 //
 //===========================================================================================================
 
-class MotionEndEvent : public BlockEvent {
+class TranslationEndEvent : public BlockEvent {
 public:
-    MotionEndEvent(uint64_t, RobotBlocksBlock *block);
-    MotionEndEvent(MotionEndEvent *ev);
-    ~MotionEndEvent();
+    TranslationEndEvent(uint64_t, BuildingBlock *block);
+    TranslationEndEvent(TranslationEndEvent *ev);
+    ~TranslationEndEvent();
     void consumeBlockEvent() {};
     void consume();
     const virtual string getEventName();
 };
 
 }
-#endif /* ROBOTBLOCKSEVENTS_H_ */
+#endif /* TRANSLATIONEVENTS_H_ */

applicationsSrc/mElection01/mElection01BlockCode.cpp
@@ -12,7 +12,7 @@
 #include <sstream>
 #include "mElection01BlockCode.h"
 #include "scheduler.h"
-#include "robotBlocksEvents.h"
+#include "translationEvents.h"
 #include <memory>
 
 const int COM_DELAY=700000;

applicationsSrc/map02/map02BlockCode.cpp
@@ -12,7 +12,7 @@
 #include <sstream>
 #include "map02BlockCode.h"
 #include "scheduler.h"
-#include "robotBlocksEvents.h"
+#include "translationEvents.h"
 #include <memory>
 
 const int COM_DELAY=1200000;

applicationsSrc/map03/map03BlockCode.cpp
@@ -9,7 +9,7 @@
 #include <sstream>
 #include "map03BlockCode.h"
 #include "scheduler.h"
-#include "robotBlocksEvents.h"
+#include "translationEvents.h"
 #include <memory>
 
 const int COM_DELAY=1000000;

applicationsSrc/map04/map04BlockCode.cpp
@@ -12,7 +12,7 @@
 #include <sstream>
 #include "map04BlockCode.h"
 #include "scheduler.h"
-#include "robotBlocksEvents.h"
+#include "translationEvents.h"
 #include <memory>
 
 const int COM_DELAY=700000;

applicationsSrc/robot01/robot01BlockCode.cpp
@@ -10,7 +10,7 @@
 
 #include "robot01BlockCode.h"
 #include "scheduler.h"
-#include "robotBlocksEvents.h"
+#include "translationEvents.h"
 #include "capabilities.h"
 
 using namespace std;
@@ -293,10 +293,10 @@ void Robot01BlockCode::processLocalEvent(EventPtr pev) {
 			finalPosition.set(robotBlock->position.pt[0]+motionVector.x,
 							  robotBlock->position.pt[1]+motionVector.y,robotBlock->position.pt[2]+motionVector.z);
 			blockToUnlock=0;
-			scheduler->schedule(new MotionStartEvent(recvMessage->globalRDVTime,robotBlock,finalPosition));
+			scheduler->schedule(new TranslationStartEvent(recvMessage->globalRDVTime,robotBlock,finalPosition));
 			stringstream info;
 			info.str("");
-			info << robotBlock->blockId << " MotionStartEvent(" << recvMessage->globalRDVTime << ")";
+			info << robotBlock->blockId << " TranslationStartEvent(" << recvMessage->globalRDVTime << ")";
 			scheduler->trace(info.str(),hostBlock->blockId,LIGHTGREY);
 			if (trainPrevious) {
 				AnswerDelayMessage *adm_message = new AnswerDelayMessage(recvMessage->globalRDVTime,false);
@@ -550,10 +550,10 @@ void Robot01BlockCode::sendAnswerDelayOrMotionDelayMessage(uint64_t gt) {
 		Vector3D finalPosition(robotBlock->position.pt[0]+motionVector.x,
 							   robotBlock->position.pt[1]+motionVector.y,
 							   robotBlock->position.pt[2]+motionVector.z);
-		scheduler->schedule(new MotionStartEvent(rdvTime,robotBlock,finalPosition));
+		scheduler->schedule(new TranslationStartEvent(rdvTime,robotBlock,finalPosition));
 		stringstream info;
 		info.str("");
-		info << robotBlock->blockId << " MotionStartEvent(" << rdvTime << ")";
+		info << robotBlock->blockId << " TranslationStartEvent(" << rdvTime << ")";
 		scheduler->trace(info.str(),hostBlock->blockId,LIGHTGREY);
 		if (trainNext) {
 			blockToUnlock=trainNext->hostBlock->blockId;

applicationsSrc/robot02/robot02BlockCode.cpp
@@ -9,7 +9,7 @@
 #include <sstream>
 #include "robot02BlockCode.h"
 #include "scheduler.h"
-#include "robotBlocksEvents.h"
+#include "translationEvents.h"
 #include <memory>
 
 

commit 0a42b26b01cd1b739ba235a3cf2fd4a8740c765b

Fix issue introduced by f00e8c64aa4cffd24f0e5ba8124eb7bbf77643d3 due to case-sensitive include

simulatorCore/src/translationEvent.h
@@ -1,90 +1,0 @@
-/**
- * @file translationEvents.h
- * @brief This file implements the realization of a translation motion events
- *  (created from refactoring of smartBlocksEvents and robotBlocksEvents)
- *
- *  Created on: 07/07/16
- *      Author: Pierre
- */
-
-#ifndef TRANSLATIONEVENTS_H_
-#define TRANSLATIONEVENTS_H_
-
-#include "BuildingBlock.h"
-#include "events.h"
-
-namespace BaseSimulator {
-
-//===========================================================================================================
-//
-//          MotionStartEvent  (class)
-//
-//===========================================================================================================
-
-class MotionStartEvent : public BlockEvent {
-    Vector3D finalPosition;
-public:
-    MotionStartEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
-    MotionStartEvent(MotionStartEvent *ev);
-    ~MotionStartEvent();
-    void consumeBlockEvent() {};
-    void consume();
-    const virtual string getEventName();
-};
-
-//===========================================================================================================
-//
-//          MotionStepEvent  (class)
-//
-//===========================================================================================================
-
-class MotionStepEvent : public BlockEvent {
-    Vector3D finalPosition;	//!< Target position of the block
-    Vector3D motionStep;		//!< Motion of the block during this step
-    Vector3D motionPosition;		//!< Actual position on the grid of the block in motion
-public:
-    MotionStepEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
-    MotionStepEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos,
-		    const Vector3D &step, const Vector3D &mpos);
-    MotionStepEvent(MotionStepEvent *ev);
-    ~MotionStepEvent();
-    void consumeBlockEvent() {};
-    void consume();
-    const virtual string getEventName();
-};
-
-//===========================================================================================================
-//
-//          MotionStopEvent  (class)
-//
-//===========================================================================================================
-
-class MotionStopEvent : public BlockEvent {
-    Vector3D finalPosition;
-public:
-    MotionStopEvent(uint64_t, BuildingBlock *block,const Vector3D &fpos);
-    MotionStopEvent(MotionStepEvent *ev);
-    ~MotionStopEvent();
-    void consumeBlockEvent() {};
-    void consume();
-    const virtual string getEventName();
-};
-
-//===========================================================================================================
-//
-//          MotionEndEvent  (class)
-//
-//===========================================================================================================
-
-class MotionEndEvent : public BlockEvent {
-public:
-    MotionEndEvent(uint64_t, BuildingBlock *block);
-    MotionEndEvent(MotionEndEvent *ev);
-    ~MotionEndEvent();
-    void consumeBlockEvent() {};
-    void consume();
-    const virtual string getEventName();
-};
-
-}
-#endif /* TRANSLATIONEVENTS_H_ */

simulatorCore/src/translationEvents.h
@@ -10,7 +10,7 @@
 #ifndef TRANSLATIONEVENTS_H_
 #define TRANSLATIONEVENTS_H_
 
-#include "BuildingBlock.h"
+#include "buildingBlock.h"
 #include "events.h"
 
 namespace BaseSimulator {