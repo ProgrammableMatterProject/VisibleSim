8e5bc77e0b208fd8a74e1d715df233cdc7fece33
Andre Naz <andre.naz01@gmail.com>
2016-08-03 16:54:02 +0200

    Remove trace printing in TargetGrid parsing.

--- simulatorCore/src/target.cpp
+++ simulatorCore/src/target.cpp
@@ -79,7 +79,6 @@ TargetGrid::TargetGrid(TiXmlNode *targetNode) : Target(targetNode) {
             cerr << "error: position attribute missing for target cell" << endl;
             throw TargetParsingException();
         }
-        cout << "Target: " << position << endl;
         attr = element->Attribute("color");
         if (attr) {
             string str(attr);
