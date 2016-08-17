01e1f155c485901710b0233323ba547dbcc8656f
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-08-17 14:26:26 +0800

    Update tests for coloration and csgCatoms3D

--- applicationsSrc/coloration/coloration_main.cpp
+++ applicationsSrc/coloration/coloration_main.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
 #include "smartBlocksSimulator.h"
 #include "smartBlocksBlockCode.h"
 #include "coloration_BlockCode.h"
@@ -18,8 +17,9 @@ using namespace SmartBlocks;
 int main(int argc, char **argv) {
 	cout << "\033[1;33m" << "Starting Smart Blocks simulation (main) ..." << "\033[0m" << endl;
 
-	srand( time(NULL));
-
+	srand(1);					/* For determinism in testing, rand() should be replaced by generator from BB
+								 * and using the seed argument */
+	
 	createSimulator(argc, argv, Coloration_BlockCode::buildNewBlockCode);
 
 	getSimulator()->printInfo();
