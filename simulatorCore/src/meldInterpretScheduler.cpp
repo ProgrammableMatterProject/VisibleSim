2c16738b5a095e7a88f09b674f4982e9c4bb6d9c
Pierre Thalamy <pierre.thalamy@gmail.com>
2018-01-04 09:26:04 +0100

    Fix signed/unsigned comparison warning in schedulers

--- simulatorCore/src/meldInterpretScheduler.cpp
+++ simulatorCore/src/meldInterpretScheduler.cpp
@@ -200,7 +200,7 @@ void *MeldInterpretScheduler::startPaused(/*void *param*/) {
 			if (!eventsMap.empty()) {
 				first=eventsMap.begin();
 				pev = (*first).second;
-				while (!eventsMap.empty() && pev->date <= chrono::duration_cast<us>(systemCurrentTimeMax).count()) {
+				while (!eventsMap.empty() && pev->date <= static_cast<uint64_t>(chrono::duration_cast<us>(systemCurrentTimeMax).count())) {
 					first=eventsMap.begin();
 					pev = (*first).second;
 					currentDate = pev->date;
