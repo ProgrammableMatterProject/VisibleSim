1cc9625097ab3c5dc3f928017bf2a2b3fe29672f
Thadeu <thadeutucci@gmail.com>
2018-06-06 21:47:48 +0200

    generate a complete cubic configuration

--- utilities/generateCubicConfig.py
+++ utilities/generateCubicConfig.py
@@ -2,22 +2,21 @@ import sys
 
 def main(argv):
     try:
-        x = int(argv[1])
+        side_length = int(argv[1])
 
-        print x
         file = open("config.xml", "w")
         file.write('<?xml version="1.0" standalone="no" ?>\n\
 <world gridSize="%d, %d, %d">\n\
-    <camera target="50,50,10" directionSpherical="-20,30,100" angle="45" near="0.01" far="250.0" />\n\
-    <spotlight target="25,25,0" directionSpherical="-35,40,150" angle="30" near="80.0" far="250.0"/>\n\
+    <camera target="50,50,10" directionSpherical="-20,30,100" angle="45" near="0.01" far="2500.0" />\n\
+    <spotlight target="600,600,420" directionSpherical="-35,30,2400" angle="30" near="80.0" far="2500.0"/>\n\
     <blockList color="128,128,128" blocksize="10,10,10">\n\n' % (x+1, x+1, x+1))
 	
-        for i in range(x):
-            for j in range(x):
-                for k in range(x):
+        for i in range(side_length):
+            for j in range(side_length):
+                for k in range(side_length):
                     file.write("\t\t<block position=\"%d,%d,%d\" orientation=\"0\"/>\n" % (i+1, j+1, k+1))
 
-        file.write('\t</blocklist>\n</world>\n')
+        file.write('\t</blockList>\n</world>\n')
         file.close()
     except:
         return False
