83448ad9c3f310bac65de6de96c747cc361d9d63
Pierre Thalamy <pierre.thalamy@gmail.com>
2019-06-27 15:25:20 +0200

    Do not send modules to EPL of branch above if tile not in CSG

--- applicationsSrc/scaffolding_csg/scaffoldingMessages.cpp
+++ applicationsSrc/scaffolding_csg/scaffoldingMessages.cpp
@@ -304,7 +304,11 @@ void RequestTargetCellMessage::handle(BaseSimulator::BlockCode* bc) {
     } else {
         // Else redirect to EPL corresponding to that branch
         // ONLY IF BRANCH HAD TO BE GROWN!
-        if (mabc.catomsReqByBranch[bi] == -1) return;
+        if (mabc.catomsReqByBranch[bi] == -1
+            or not mabc.ruleMatcher->
+            isInCSG(mabc.ruleMatcher->
+                    getTileRootAtEndOfBranch(mabc.norm(mabc.catom->position), bi))
+            ) return;
 
         tPos = mabc.catom->position + ScaffoldingRuleMatcher::getTargetEPLPositionForBranch(bi);
     }
