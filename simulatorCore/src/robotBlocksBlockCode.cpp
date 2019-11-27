e722a0887f53907ef1012c8ecae28a83b2f77d24
Pierre Thalamy <pierre.thalamy@gmail.com>
2019-11-27 12:32:06 +0100

    Add missing robotBlock processLocalEvent refactoring

--- simulatorCore/src/robotBlocksBlockCode.cpp
+++ simulatorCore/src/robotBlocksBlockCode.cpp
@@ -14,13 +14,13 @@ using namespace std;
 namespace RobotBlocks {
 RobotBlocksBlockCode::RobotBlocksBlockCode(RobotBlocksBlock *host):BlockCode(host) {
 #ifdef DEBUG_OBJECT_LIFECYCLE
-	OUTPUT << "RobotBlocksBlockCode constructor" << endl;
+    OUTPUT << "RobotBlocksBlockCode constructor" << endl;
 #endif
 }
 
 RobotBlocksBlockCode::~RobotBlocksBlockCode() {
-#ifdef DEBUG_OBJECT_LIFECYCLE    
-	OUTPUT << "RobotBlocksBlockCode destructor" << endl;
+#ifdef DEBUG_OBJECT_LIFECYCLE
+    OUTPUT << "RobotBlocksBlockCode destructor" << endl;
 #endif
 }
 
@@ -28,26 +28,18 @@ void RobotBlocksBlockCode::processLocalEvent(EventPtr pev) {
     MessagePtr message;
     stringstream info;
 
-//cout << "event #" << pev->id << ":" << pev->eventType << endl;
+    BlockCode::processLocalEvent(pev);
+
     switch (pev->eventType) {
-        case EVENT_NI_RECEIVE: {
-            message = (std::static_pointer_cast<NetworkInterfaceReceiveEvent>(pev))->message;
-    // search message id in eventFuncMap
-            multimap<int,eventFunc>::iterator im = eventFuncMap.find(message->type);
-            if (im!=eventFuncMap.end()) {
-                P2PNetworkInterface *recv_interface = message->destinationInterface;
-                (*im).second(this,message,recv_interface);
-            } else {
-                OUTPUT << "ERROR: message Id #" << message->type << " unknown!" << endl;
-            }
-        } break;
-        case EVENT_ADD_NEIGHBOR: {
-            OUTPUT << "ADD_NEIGHBOR" << endl;
-        } break;
-        case EVENT_TAP: {
-			int face = (std::static_pointer_cast<TapEvent>(pev))->tappedFace;
-            onTap(face);
-        } break;
+        case EVENT_TRANSLATION_END: {
+#ifdef verbose
+            info.str("");
+            info << "rec.: EVENT_MOTION_END";
+            scheduler->trace(info.str(),hostBlock->blockId);
+#endif
+            onMotionEnd();
+        }  break;
     }
 }
+
 }
