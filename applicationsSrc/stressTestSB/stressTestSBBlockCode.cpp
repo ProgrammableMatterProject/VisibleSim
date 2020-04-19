eab619e978e367220a2c74ff5268636938276b1d
Pierre Thalamy <pierre.thalamy@gmail.com>
2020-04-19 15:14:47 +0200

    StressTest - Fix modules that stopped moving forever when no motion was possible

--- applicationsSrc/stressTestSB/stressTestSBBlockCode.cpp
+++ applicationsSrc/stressTestSB/stressTestSBBlockCode.cpp
@@ -25,7 +25,7 @@ StressTestSBBlockCode::StressTestSBBlockCode(SmartBlocksBlock *host) : SmartBloc
     // Set the module pointer
     module = static_cast<SmartBlocksBlock*>(hostBlock);
     lattice = BaseSimulator::getWorld()->lattice;
-  }
+}
 
 void StressTestSBBlockCode::startup() {
     rng.seed(BaseSimulator::Simulator::getSimulator()->getCmdLine().getSimulationSeed());
@@ -35,8 +35,8 @@ void StressTestSBBlockCode::startup() {
     // Leader initiates activation
     if (module->blockId == 1) { // Master ID is 1
         module->setColor(BLUE);
-				setLockedCell(module->position, true);
-				sendMessageToAllNeighbors("Activate", new Message(ACTIVATION_MSG_ID),100,200,0);
+        setLockedCell(module->position, true);
+        sendMessageToAllNeighbors("Activate", new Message(ACTIVATION_MSG_ID),100,200,0);
         wait();
     } else {
         hostBlock->setColor(YELLOW);
@@ -94,7 +94,13 @@ void StressTestSBBlockCode::processLocalEvent(std::shared_ptr<Event> pev) {
             switch(itev->mode) {
                 case AGITATE_IT_ID:
                     console << " agitate" << "\n";
-                    if (!isMoving) randomWalk();
+                    if (!isMoving) {
+                        if (not randomWalk()) { // Wait .5s and try moving again
+                            wait();
+                        } else {
+                            module->setColor(BLUE);
+                        }
+                    }
                     break;
             }
         } break;
@@ -114,10 +120,10 @@ bool StressTestSBBlockCode::randomWalk() {
                      return not isLockedCell(p) and module->canMoveTo(p); });
 
     if (candidates.empty()) {
-			module->setColor(RED);
-			activated = false;
-			return false;
-		}
+        module->setColor(RED);
+        return false;
+    }
+
     // Randomly pick one candidate
     std::random_shuffle ( candidates.begin(), candidates.end() );
 
@@ -125,8 +131,8 @@ bool StressTestSBBlockCode::randomWalk() {
     const Cell3DPosition& dest = candidates.front();
     lastPos = module->position;
     setLockedCell(dest, true);
-		console << "Moveto" << dest << "\n";
-		isMoving=true;
+    console << "Moveto" << dest << "\n";
+    isMoving = true;
     module->moveTo(dest);
 
     return true;
@@ -177,26 +183,25 @@ void StressTestSBBlockCode::onBlockSelected() {
 }
 
 void StressTestSBBlockCode::onGlDraw() {
-	static const float color[4]={0.2f,0.2f,0.2f,1.0f};
-	if (lockedCells) {
-		const Cell3DPosition& gs = lattice->gridSize;
-		const Vector3D gl = lattice->gridScale;
-		bool *ptr=lockedCells;
-		glDisable(GL_TEXTURE);
-		glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,color);
-		for (int iy=0; iy<gs[1]; iy++) {
-			for (int ix=0; ix<gs[0]; ix++) {
-				if (*ptr) {
-					glPushMatrix();
-					glTranslatef((ix+0.5f)*gl[0],(iy+0.5f)*gl[1],10.0);
-					glScalef(3,3,10);
-					glutSolidCube(1.0);
-					glPopMatrix();
-				}
-				ptr++;	
-			}
-		}
-		
-	}
-}
+    static const float color[4]={0.2f,0.2f,0.2f,1.0f};
+    if (lockedCells) {
+        const Cell3DPosition& gs = lattice->gridSize;
+        const Vector3D gl = lattice->gridScale;
+        bool *ptr=lockedCells;
+        glDisable(GL_TEXTURE);
+        glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,color);
+        for (int iy=0; iy<gs[1]; iy++) {
+            for (int ix=0; ix<gs[0]; ix++) {
+                if (*ptr) {
+                    glPushMatrix();
+                    glTranslatef((ix+0.5f)*gl[0],(iy+0.5f)*gl[1],10.0);
+                    glScalef(3,3,10);
+                    glutSolidCube(1.0);
+                    glPopMatrix();
+                }
+                ptr++;
+            }
+        }
 
+    }
+}
