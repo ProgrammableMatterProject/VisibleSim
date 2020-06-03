f63ecc6bc64ef24b8954193d6f3c25d4ec4057ed
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2020-06-03 16:40:51 +0200

    First version : crash inexplicably

--- applicationsSrc/tetris/tetrisCode.cpp
+++ applicationsSrc/tetris/tetrisCode.cpp
@@ -1,72 +1,140 @@
 #include "tetrisCode.hpp"
 
-TetrisCode::TetrisCode(BlinkyBlocksBlock *host):BlinkyBlocksBlockCode(host),module(host) {
+TetrisCode::TetrisCode(BlinkyBlocksBlock *host) : BlinkyBlocksBlockCode(host), module(host)
+{
     // @warning Do not remove block below, as a blockcode with a NULL host might be created
     //  for command line parsing
-    if (not host) return;
+    if (not host)
+        return;
 
     // Registers a callback (myHeightMsgFunc) to the message of type E
     addMessageEventFunc2(HEIGHTMSG_MSG_ID,
-                       std::bind(&TetrisCode::myHeightMsgFunc,this,
-                       std::placeholders::_1, std::placeholders::_2));
+                         std::bind(&TetrisCode::myHeightMsgFunc, this,
+                                   std::placeholders::_1, std::placeholders::_2));
 
     // Registers a callback (myWidthMsgFunc) to the message of type D
     addMessageEventFunc2(WIDTHMSG_MSG_ID,
-                       std::bind(&TetrisCode::myWidthMsgFunc,this,
-                       std::placeholders::_1, std::placeholders::_2));
-
+                         std::bind(&TetrisCode::myWidthMsgFunc, this,
+                                   std::placeholders::_1, std::placeholders::_2));
 }
 
-void TetrisCode::startup() {
+void TetrisCode::startup()
+{
     console << "start " << module->blockId << "\n";
-    if (module->blockId == 1) { // Master id is 1
-        module->setColor(RED);
-	}
+
+    P2PNetworkInterface *topItf = module->getInterface(SCLattice::Direction::Top);
+    P2PNetworkInterface *bottomItf = module->getInterface(SCLattice::Direction::Bottom);
+    P2PNetworkInterface *rightItf = module->getInterface(SCLattice::Direction::Right);
+    P2PNetworkInterface *leftItf = module->getInterface(SCLattice::Direction::Left);
+
+    sendHeight();
+    sendWidth();
+
+    module->setColor(Colors[(height+width)%NB_COLORS]);
 }
-void TetrisCode::myHeightMsgFunc(std::shared_ptr<Message>_msg, P2PNetworkInterface*sender) {
 
-    MessageOf<int>* msg = static_cast<MessageOf<int>*>(_msg.get());
+void TetrisCode::sendHeight()
+{
+    if (topItf != nullptr and topItf->isConnected())
+    {
+        sendMessage("Height Message", new MessageOf<int>(HEIGHTMSG_MSG_ID, height + 1), topItf, 0, 0);
+    }
+    if (bottomItf != nullptr and bottomItf->isConnected() and height > 0) // negative height is not send
+    {
+        sendMessage("Height Message", new MessageOf<int>(HEIGHTMSG_MSG_ID, height - 1), bottomItf, 0, 0);
+    }
+    if (rightItf != nullptr and rightItf->isConnected())
+    {
+        sendMessage("Height Message", new MessageOf<int>(HEIGHTMSG_MSG_ID, height), rightItf, 0, 0);
+    }
+    if (leftItf != nullptr and leftItf->isConnected())
+    {
+        sendMessage("Height Message", new MessageOf<int>(HEIGHTMSG_MSG_ID, height), leftItf, 0, 0);
+    }
+}
+
+void TetrisCode::sendWidth()
+{
+    if (topItf != nullptr and topItf->isConnected())
+    {
+        sendMessage("Width Message", new MessageOf<int>(WIDTHMSG_MSG_ID, width), topItf, 0, 0);
+    }
+    if (bottomItf != nullptr and bottomItf->isConnected())
+    {
+        sendMessage("Width Message", new MessageOf<int>(WIDTHMSG_MSG_ID, width), bottomItf, 0, 0);
+    }
+    if (rightItf != nullptr and rightItf->isConnected())
+    {
+        sendMessage("Width Message", new MessageOf<int>(WIDTHMSG_MSG_ID, width + 1), rightItf, 0, 0);
+    }
+    if (leftItf != nullptr and leftItf->isConnected() and width > 0) // negative width is not send
+    {
+        sendMessage("Width Message", new MessageOf<int>(WIDTHMSG_MSG_ID, width - 1), leftItf, 0, 0);
+    }
+}
+void TetrisCode::myHeightMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender)
+{
+
+    MessageOf<int> *msg = static_cast<MessageOf<int> *>(_msg.get());
     int msgData = *msg->getData();
-
+    if (height < msgData)
+    {
+        height = msgData;
+        module->setColor(Colors[(height+width)%NB_COLORS]);
+        sendHeight();
+    }
 };
 
-void TetrisCode::myWidthMsgFunc(std::shared_ptr<Message>_msg, P2PNetworkInterface*sender) {
+void TetrisCode::myWidthMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender)
+{
 
-    MessageOf<int>* msg = static_cast<MessageOf<int>*>(_msg.get());
+    MessageOf<int> *msg = static_cast<MessageOf<int> *>(_msg.get());
     int msgData = *msg->getData();
 
+    if (width < msgData)
+    {
+        width = msgData;
+        module->setColor(Colors[(height+width)%NB_COLORS]);
+        sendWidth();
+    }
 };
 
-
-void TetrisCode::processLocalEvent(std::shared_ptr<Event> pev) {
+void TetrisCode::processLocalEvent(std::shared_ptr<Event> pev)
+{
     std::shared_ptr<Message> message;
     stringstream info;
 
     // Do not remove line below
     BlinkyBlocksBlockCode::processLocalEvent(pev);
 
-    switch (pev->eventType) {
-        case EVENT_ADD_NEIGHBOR: {
-            // Do something when a neighbor is added to an interface of the module
-            break;
-        }
+    switch (pev->eventType)
+    {
+    case EVENT_ADD_NEIGHBOR:
+    {
+        // Do something when a neighbor is added to an interface of the module
+        break;
+    }
 
-        case EVENT_REMOVE_NEIGHBOR: {
-            // Do something when a neighbor is removed from an interface of the module
-            break;
-        }
+    case EVENT_REMOVE_NEIGHBOR:
+    {
+        // Do something when a neighbor is removed from an interface of the module
+        break;
+    }
 
-        case EVENT_INTERRUPTION: {
-            // Do something when the module receives an event
-            break;
-        }
+    case EVENT_INTERRUPTION:
+    {
+        // Do something when the module receives an event
+        break;
+    }
     break;
     }
 };
 
-void TetrisCode::onUserKeyPressed(unsigned char c, int x, int y) {
-    switch (c) {
-        case 'a' : // update with your code
+void TetrisCode::onUserKeyPressed(unsigned char c, int x, int y)
+{
+    switch (c)
+    {
+    case 'a': // update with your code
         break;
     }
 };
--- applicationsSrc/tetris/tetrisCode.hpp
+++ applicationsSrc/tetris/tetrisCode.hpp
@@ -10,41 +10,59 @@ static const int WIDTHMSG_MSG_ID = 1002;
 
 using namespace BlinkyBlocks;
 
-class TetrisCode : public BlinkyBlocksBlockCode {
+class TetrisCode : public BlinkyBlocksBlockCode
+{
 private:
-	BlinkyBlocksBlock *module = nullptr;
-public :
-	TetrisCode(BlinkyBlocksBlock *host);
-	~TetrisCode() {};
+    BlinkyBlocksBlock *module = nullptr;
+    P2PNetworkInterface *topItf;
+    P2PNetworkInterface *bottomItf;
+    P2PNetworkInterface *rightItf;
+    P2PNetworkInterface *leftItf;
+    int height = 0; //"vertical" coordinate of the module -> to initialize
+    int width = 0;  //"horizontal" coordinate of the module -> to initialize
 
-/**
+public:
+    TetrisCode(BlinkyBlocksBlock *host);
+    ~TetrisCode(){};
+
+    /**
   * This function is called on startup of the blockCode, it can be used to perform initial
   *  configuration of the host or this instance of the program.
   * @note this can be thought of as the main function of the module
   */
     void startup() override;
 
-/**
+    /**
+  * @brief Message sender of height to all neighbors -> to initialize
+  */
+    void sendHeight();
+
+    /**
+  * @brief Message sender of width to all neighbors -> to initialize
+  */
+    void sendWidth();
+
+    /**
   * @brief Message handler for the message 'heightMsg'
   * @param _msg Pointer to the message received by the module, requires casting
   * @param sender Connector of the module that has received the message and that is connected to the sender
   */
-   void myHeightMsgFunc(std::shared_ptr<Message>_msg,P2PNetworkInterface *sender);
+    void myHeightMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
 
-/**
+    /**
   * @brief Message handler for the message 'widthMsg'
   * @param _msg Pointer to the message received by the module, requires casting
   * @param sender Connector of the module that has received the message and that is connected to the sender
   */
-   void myWidthMsgFunc(std::shared_ptr<Message>_msg,P2PNetworkInterface *sender);
+    void myWidthMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
 
-/**
+    /**
   * @brief Handler for all events received by the host block
   * @param pev pointer to the received event
   */
     void processLocalEvent(std::shared_ptr<Event> pev) override;
 
-/**
+    /**
   * User-implemented keyboard handler function that gets called when
   *  a key press event could not be caught by openglViewer
   * @param c key that was pressed (see openglViewer.cpp)
@@ -54,12 +72,13 @@ public :
   */
     void onUserKeyPressed(unsigned char c, int x, int y) override;
 
-/*****************************************************************************/
-/** needed to associate code to module                                      **/
-	static BlockCode *buildNewBlockCode(BuildingBlock *host) {
-	    return(new TetrisCode((BlinkyBlocksBlock*)host));
-	};
-/*****************************************************************************/
+    /*****************************************************************************/
+    /** needed to associate code to module                                      **/
+    static BlockCode *buildNewBlockCode(BuildingBlock *host)
+    {
+        return (new TetrisCode((BlinkyBlocksBlock *)host));
+    };
+    /*****************************************************************************/
 };
 
 #endif /* tetrisCode_H_ */
