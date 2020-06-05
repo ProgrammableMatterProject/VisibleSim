d89c4d05bcd959a6bb46c8481cca49a4e56a8d07
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2020-06-05 09:24:36 +0200

    Propagation of the first tetramino (square)

--- applicationsSrc/tetris/tetrisCode.cpp
+++ applicationsSrc/tetris/tetrisCode.cpp
@@ -1,5 +1,7 @@
 #include "tetrisCode.hpp"
+#include "utils/random.h"
 #include <iostream>
+#include <array>
 
 TetrisCode::TetrisCode(BlinkyBlocksBlock *host) : BlinkyBlocksBlockCode(host), module(host)
 {
@@ -27,6 +29,14 @@ TetrisCode::TetrisCode(BlinkyBlocksBlock *host) : BlinkyBlocksBlockCode(host), m
     addMessageEventFunc2(MAXWIDTHMSG_MSG_ID,
                          std::bind(&TetrisCode::myMaxWidthMsgFunc, this,
                                    std::placeholders::_1, std::placeholders::_2));
+
+    // Registers a callback (mySendTmn1Func) to the message of type E
+    addMessageEventFunc2(TMN1_MSG_ID,
+                         std::bind(&TetrisCode::myTmn1Func, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+
+    // Initialization of random numbers generator
+    srand(Random::getSimulationSeed());
 }
 
 void TetrisCode::startup()
@@ -39,8 +49,7 @@ void TetrisCode::startup()
 
     sendHeight();
     sendWidth();
-
-    module->setColor(Colors[(height + width) % NB_COLORS]);
+    module->setColor(Colors[color]);
 }
 
 void TetrisCode::sendHeight()
@@ -107,7 +116,7 @@ int TetrisCode::pixelCalculation()
 {
     if (maxHeight < MIN_HEIGHT || maxWidth < MIN_WIDTH) //The set is too small to display a tetris game
     {
-        return 0 ;
+        return 0;
     }
 
     int sizeOfPixel = maxHeight / MIN_HEIGHT;
@@ -160,8 +169,33 @@ int TetrisCode::pixelCalculation()
             roleInPixel = CORE;
         }
     }
-    module->setColor(Colors[(pixelHCoord + pixelWCoord) % NB_COLORS]);
-    return 1 ;
+
+    int totalHNbPixels = maxHeight / sizeOfPixel; //the number of (full) pixels that are displayed on height
+    int totalWNbPixels = maxWidth / sizeOfPixel;  //the number of (full) pixels that are displayed on width
+    //The tetraminos appear at the top of the set, in the middle
+    if (height == (totalHNbPixels - 1) * sizeOfPixel && width == (totalWNbPixels / 2) * sizeOfPixel)
+    {
+        appear_module = true;
+    }
+    else
+    {
+        appear_module = false;
+    }
+    return 1;
+}
+
+void TetrisCode::tmnAppearance()
+{
+    int r = (int)rand();
+    tmn = r % 7 + 1;
+    r = (int)rand();
+    rotation = r % 4 + 1;
+    while (color == NO_COLOR)
+    {
+        r = (int)rand();
+        color = r % 9;
+    }
+    module->setColor(Colors[color]);
 }
 
 void TetrisCode::myHeightMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender)
@@ -178,6 +212,7 @@ void TetrisCode::myHeightMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterf
     {
         maxHeight = height;
         sendIntToAll(MAXHEIGHTMSG_MSG_ID, maxHeight);
+        pixelCalculation();
     }
 };
 
@@ -196,6 +231,7 @@ void TetrisCode::myWidthMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterfa
     {
         maxWidth = width;
         sendIntToAll(MAXWIDTHMSG_MSG_ID, maxWidth);
+        pixelCalculation();
     }
 };
 
@@ -223,6 +259,129 @@ void TetrisCode::myMaxWidthMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInte
         maxWidth = msgData;
         sendIntToAll(MAXWIDTHMSG_MSG_ID, maxWidth);
         pixelCalculation();
+    }
+};
+
+void TetrisCode::sendTmn1() // NB : the first tetramino doesn't rotate (square)
+{
+    std::array<int> data[4] = {rotation, position, color};
+    if (roleInPixel == TOP_BORDER || roleInPixel == TOP_LEFT_CORNER || roleInPixel == TOP_RIGHT_CORNER)
+    {
+        if (position == 3)
+        {
+            data[1] = 1; //If the position is 3, the position sent to the top pixel is 1.
+        }
+        else if (position == 4)
+        {
+            data[1] = 2;
+        }
+        if (position == 3 || position == 4) //The pixels in position 1 and 2 doesn't spread the data to their top pixel
+        {
+            if (topItf != nullptr and topItf->isConnected())
+            {
+                sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), topItf, 0, 0);
+            }
+        }
+    }
+    else
+    {
+        if (topItf != nullptr and topItf->isConnected())
+        {
+            sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), topItf, 0, 0);
+        }
+    }
+    data[1] = position;
+    if (roleInPixel == BOTTOM_BORDER || roleInPixel == BOTTOM_LEFT_CORNER || roleInPixel == BOTTOM_RIGHT_CORNER)
+    {
+        if (position == 1)
+        {
+            data[1] = 3; //If the position is 1, the position sent to the bottom pixel is 3.
+        }
+        else if (position == 2)
+        {
+            data[1] = 4;
+        }
+        if (position == 1 || position == 2) //The pixels in position 3 and 4 doesn't spread the data to their bottom pixel
+        {
+            if (bottomItf != nullptr and bottomItf->isConnected())
+            {
+                sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), bottomItf, 0, 0);
+            }
+        }
+    }
+    else
+    {
+        if (bottomItf != nullptr and bottomItf->isConnected())
+        {
+            sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), bottomItf, 0, 0);
+        }
+    }
+    data[1] = position;
+    if (roleInPixel == RIGHT_BORDER || roleInPixel == TOP_RIGHT_CORNER || roleInPixel == BOTTOM_RIGHT_CORNER)
+    {
+        if (position == 1)
+        {
+            data[1] = 2;
+        }
+        else if (position == 3)
+        {
+            data[1] = 4;
+        }
+        if (position == 1 || position == 3)
+        {
+            if (rightItf != nullptr and rightItf->isConnected())
+            {
+                sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), rightItf, 0, 0);
+            }
+        }
+    }
+    else
+    {
+        if (rightItf != nullptr and rightItf->isConnected())
+        {
+            sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), rightItf, 0, 0);
+        }
+    }
+    data[1] = position;
+    if (roleInPixel == LEFT_BORDER || roleInPixel == TOP_LEFT_CORNER || roleInPixel == BOTTOM_LEFT_CORNER)
+    {
+        if (position == 2)
+        {
+            data[1] = 1;
+        }
+        else if (position == 4)
+        {
+            data[1] = 3;
+        }
+        if (position == 2 || position == 4)
+        {
+            if (leftItf != nullptr and leftItf->isConnected())
+            {
+                sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), leftItf, 0, 0);
+            }
+        }
+    }
+    else
+    {
+        if (leftItf != nullptr and leftItf->isConnected())
+        {
+            sendMessage("Tmn 1 Message", new MessageOf<std::array>(TMN1_MSG_ID, data), leftItf, 0, 0);
+        }
+    }
+}
+
+void TetrisCode::myTmn1Func(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender)
+{
+
+    MessageOf<std::array> *msg = static_cast<MessageOf<std::array> *>(_msg.get());
+    std::array msgData = *msg->getData();
+    if(rotation != msgData[0] || position != msgdata[1] || color != msgData[2])
+    {
+        rotation = msgData[0];
+        position = msgdata[1];
+        color = msgData[2]
+        module->setColor(Color[color]);
+        sendTmn1();
     }
 };
 
--- applicationsSrc/tetris/tetrisCode.hpp
+++ applicationsSrc/tetris/tetrisCode.hpp
@@ -4,10 +4,11 @@
 #include "robots/blinkyBlocks/blinkyBlocksSimulator.h"
 #include "robots/blinkyBlocks/blinkyBlocksWorld.h"
 #include "robots/blinkyBlocks/blinkyBlocksBlockCode.h"
+#include "utils/color.h"
 
 // Possible roles of modules in a pixel
-static const int ALONE = 0 ; //if the pixel's size is 1x1
-static const int CORE = 1; //all modules that are not on the border of the pixel
+static const int ALONE = 0; //if the pixel's size is 1x1
+static const int CORE = 1;  //all modules that are not on the border of the pixel
 static const int TOP_BORDER = 2;
 static const int TOP_RIGHT_CORNER = 3;
 static const int RIGHT_BORDER = 4;
@@ -18,14 +19,95 @@ static const int LEFT_BORDER = 8;
 static const int TOP_LEFT_CORNER = 9;
 
 //Minimum size of the BB set to display a tetris game (& min nb of pixels on each dimension)
-static const int MIN_HEIGHT = 10 ; 
-static const int MIN_WIDTH = 3 ;
+static const int MIN_HEIGHT = 10;
+static const int MIN_WIDTH = 3;
+
+//Types of tetramino
+static const int NO_TMN = 0; //if the module doesn't belong to any tetramino
+//All the types of tetramino considered here, and the numbers associated with the positions of the pixels :
+static const int NO_POSITION = 0;
+/*
+NB : The position 1 is the position of the "center" of the tetramino, that means it is the position of the pixel that
+"creates" the tetramino AND the center of rotation, except for the first tetramino that doesn't rotate.
+
+Tetramino 1
+ --- ---
+| 1 | 2 |
+ --- ---
+| 3 | 4 |
+ --- ---
+
+Tetramino 2 
+ ---
+| 2 |
+ ---
+| 1 |
+ ---
+| 3 |
+ ---
+| 4 |
+ ---
+
+Tetramino 3
+ ---
+| 4 |
+ ---
+| 3 |
+ --- ---
+| 1 | 2 |
+ --- ---
+
+Tetramino 4
+     ---
+    | 4 |
+     ---
+    | 3 |
+ --- ---
+| 2 | 1 |
+ --- ---
+
+Tetramino 5
+     ---
+    | 3 |
+ --- --- ---
+| 2 | 1 | 4 |
+ --- --- ---
+
+Tetramino 6
+ ---
+| 2 |
+ --- ---
+| 1 | 3 |
+ --- ---
+    | 4 |
+     ---
+
+Tetramino 7
+     ---
+    | 2 |
+ --- ---
+| 3 | 1 |
+ --- ---
+| 4 |
+ ---
+
+*/
+
+//Types of rotations
+static const int NO_ROTATION = 0; //if the module belongs to no tetramino or the rotation wasn't defined
+static const int NORTH = 1;       //the tetramino is upright
+static const int EAST = 2;        //the top of the tetramino is on the right
+static const int SOUTH = 3;       //the tetramino is upside down
+static const int WEST = 4;        //the top of the tetramino is on the left
+
+static const int NO_COLOR = 6; //color if the module doesn't belong to any tetramino => WHITE
 
 //Message IDs
 static const int HEIGHTMSG_MSG_ID = 1001;
 static const int WIDTHMSG_MSG_ID = 1002;
 static const int MAXHEIGHTMSG_MSG_ID = 1003;
 static const int MAXWIDTHMSG_MSG_ID = 1004;
+static const int TMN1_MSG_ID = 1005;
 
 using namespace BlinkyBlocks;
 
@@ -37,14 +119,20 @@ private:
     P2PNetworkInterface *bottomItf = nullptr;
     P2PNetworkInterface *rightItf = nullptr;
     P2PNetworkInterface *leftItf = nullptr;
-    int height = 0;        //"vertical" coordinate of the module -> to initialize
-    int width = 0;         //"horizontal" coordinate of the module -> to initialize
-    int maxHeight = 0;     //maximum height of the BBs set
-    int maxWidth = 0;      //maximum width of the BBs set
-    int pixelHCoord = 0;   //"vertical" coordinate of the pixel the module belongs to
-    int pixelWCoord = 0;   //"horizontal" coordinate of the pixel the module belongs to
-    int roleInPixel = 100; // role of the module in the pixel (core, border, corner)
+    int height = 0;             //"vertical" coordinate of the module -> to initialize
+    int width = 0;              //"horizontal" coordinate of the module -> to initialize
+    int maxHeight = 0;          //maximum height of the BBs set
+    int maxWidth = 0;           //maximum width of the BBs set
+    int pixelHCoord = 0;        //"vertical" coordinate of the pixel the module belongs to
+    int pixelWCoord = 0;        //"horizontal" coordinate of the pixel the module belongs to
+    int roleInPixel = 100;      // role of the module in the pixel (core, border, corner)
+    bool appear_module = false; //true if the module is the one which picks the form, rotation and color of the new tetramino
 
+    bool belongsToTmn = false;  //true if the module is part of a tetramino
+    int tmn = NO_TMN;           // represents the type of tetramino the module is part of
+    int rotation = NO_ROTATION; //represents the rotation of the tetramino the module is part of
+    int position = 0;           //reprensents the position of the pixel in the tetramino the module is part of
+    int color = NO_COLOR;       //represents the color of the tetramino the module is part of
 public:
     TetrisCode(BlinkyBlocksBlock *host);
     ~TetrisCode(){};
@@ -83,6 +171,12 @@ public:
     int pixelCalculation();
 
     /**
+    @brief creates a tetramino and starts to spread it
+    
+    */
+    void tmnAppearance();
+
+    /**
   * @brief Message handler for the message 'heightMsg'
   * @param _msg Pointer to the message received by the module, requires casting
   * @param sender Connector of the module that has received the message and that is connected to the sender
@@ -109,6 +203,18 @@ public:
   * @param sender Connector of the module that has received the message and that is connected to the sender
   */
     void myMaxWidthMsgFunc(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+
+    /**
+  * @brief spread the first tetramino
+  */
+    void sendTmn1();
+
+    /**
+  * @brief Message handler for the message 'tmn1'
+  * @param _msg Pointer to the message received by the module, requires casting
+  * @param sender Connector of the module that has received the message and that is connected to the sender
+  */
+    void myTmn1Func(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
 
     /**
   * @brief Handler for all events received by the host block
