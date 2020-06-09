eee23285f7ebd5b78b825b2ab5bda4f782f0b62a
Pierre Thalamy <pierre.thalamy@gmail.com>
2020-06-09 11:17:24 +0200

    Add exporter files

---
+++ simulatorCore/src/player/exporter.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file   exporter.cpp
+ * @author Pierre Thalamy <pthalamy@pierre-ZenBook-UX433FA>
+ * @date   Tue Jun  9 11:14:00 2020
+ *
+ * @brief Simulation data exporter to be used for simulation replay
+ *
+ *
+ */
