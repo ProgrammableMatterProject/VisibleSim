259cee139506556027749784f2c12b806fb175ff
Pierre Thalamy <pierre.thalamy@gmail.com>
2020-06-10 11:35:11 +0200

    Fix parseUserBlockElements not getting called for several module architectures

--- simulatorCore/src/robots/nodes2D/nodes2DSimulator.cpp
+++ simulatorCore/src/robots/nodes2D/nodes2DSimulator.cpp
@@ -74,6 +74,7 @@ void Nodes2DSimulator::loadBlock(TiXmlElement *blockElt, bID blockId, BlockCodeB
 
     // Finally, add block to the world
     ((Nodes2DWorld*)world)->addBlock(blockId, bcb, pos, color, orientation, master);
+    world->getBlockById(blockId)->blockCode->parseUserBlockElements(blockElt);
 }
 
 } // Nodes2D namespace
--- simulatorCore/src/robots/slidingCubes/slidingCubesSimulator.cpp
+++ simulatorCore/src/robots/slidingCubes/slidingCubesSimulator.cpp
@@ -60,6 +60,7 @@ void SlidingCubesSimulator::loadBlock(TiXmlElement *blockElt, bID blockId, Block
 
     // Finally, add block to the world
     ((SlidingCubesWorld*)world)->addBlock(blockId, bcb, pos, color, 0, master);
+    world->getBlockById(blockId)->blockCode->parseUserBlockElements(blockElt);
 }
 
 } // SlidingCubes namespace
