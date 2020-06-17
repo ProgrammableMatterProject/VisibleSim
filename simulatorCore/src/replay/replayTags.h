617dad46a0f2f4a8e353302c4059cf3aaaecce1d
Pierre Thalamy <pierre.thalamy@gmail.com>
2020-06-17 16:51:01 +0200

    Change sizeof(streampos) to sizeof(s8) in index export

--- simulatorCore/src/replay/replayTags.h
+++ simulatorCore/src/replay/replayTags.h
@@ -18,6 +18,11 @@ typedef std::uint16_t u2;
 typedef std::uint32_t u4;
 typedef std::uint64_t u8;
 
+typedef std::int8_t s1; // [s]igned 1
+typedef std::int16_t s2;
+typedef std::int32_t s4;
+typedef std::int64_t s8;
+
 /* GENERAL FLAGS */
 
 /**
