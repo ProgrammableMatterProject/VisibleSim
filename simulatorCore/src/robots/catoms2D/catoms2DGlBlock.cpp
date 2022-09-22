2b6a9b0b5275ddf0546d09acf927b0011efb7bbb
docben <benoit.piranda@gmail.com>
2022-09-22 10:23:46 +0200

    Catom2D glBlock error

--- simulatorCore/src/robots/catoms2D/catoms2DGlBlock.cpp
+++ simulatorCore/src/robots/catoms2D/catoms2DGlBlock.cpp
@@ -6,7 +6,7 @@ void Catoms2DGlBlock::glDraw(ObjLoader::ObjLoader *ptrObj) {
     glPushMatrix();
     glTranslatef(position[0],position[1],position[2]);
     glRotatef(-angle,0,1,0);
-    if (highlighted()) {
+    if (highlighted) {
         GLfloat n = 0.5+1.5*(1.0-(glutGet(GLUT_ELAPSED_TIME)%1000)/1000.0);
         GLfloat c[4];
         c[0]=color[0]*n;
